2572d8c03d75137dc571ea078b15f7762c094b90
BruceWayne52411 <158220789+BruceWayne52411@users.noreply.github.com>
2024-01-30 14:31:07 -0700

    Initail
    Added slideshow

---
+++ js/site.js
@@ -0,0 +1,54 @@
+console.log("js loaded")
+
+// setInterval will call a function every certain time(r)
+// every Nms
+
+// let count = 0;
+
+// function printCountAndIncrement(){
+//     count++;
+//     console.log(count);
+// }
+
+// //call our function every 3 seconds
+// setInterval(printCountAndIncrement, 3000);
+
+let imageUrls = [
+    "./images/batman1.jpg",
+    "./images/batman2.jpg",
+    "./images/batman3.jpg"
+]
+
+const prevButton = document.getElementById("prev-button")
+const nextButton = document.getElementById("next-button")
+
+prevButton.addEventListener("click", () => {
+    updateImage("prev");
+})
+
+nextButton.addEventListener("click", () => {
+    updateImage("next");
+})
+
+const slideShowImage = document.getElementById("slide-show-image");
+
+let currentIndexImage = 0;
+
+// Move to the next image update the dom to show keep track of the current index
+function updateImage(direction){
+    if (direction == "next"){
+        currentIndexImage++;
+    } else if("prev"){
+        currentIndexImage--;
+    } else (currentIndexImage++)
+    if (currentIndexImage >= imageUrls.length){
+        currentIndexImage = 0;
+    }
+    if(currentIndexImage < 0){
+        currentIndexImage = imageUrls[currentIndexImage]
+    }
+
+    slideShowImage.src = imageUrls[currentIndexImage];
+}
+
+setInterval(updateImage, 5000);
